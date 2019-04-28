a4713d991d90365c5b358e5fde4b97e640efbc67
George <admin@georgesimos.com>
2019-04-28 11:48:47 +0300

    adding mongoose for localhost connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
-
+require('./db/mongoose')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World'))
 
-app.listen(3000)
+app.listen(port, () => console.log(`app is running in PORT: ${port}`))
