c8ec1a5a5e81908f9ab26f2d9bac68ac17266495
George <admin@georgesimos.com>
2019-05-01 19:41:28 +0300

    login and logout

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,13 +4,37 @@ const auth = require('../middlewares/auth')
 
 const router = new express.Router()
 
-//Create a user
+// Create a user
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Login User
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.username, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Logout user
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
     } catch (e) {
         res.status(400).send(e)
     }
