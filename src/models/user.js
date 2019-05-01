dcd70b578215f11cca93ab1f4193b037a0080208
George <admin@georgesimos.com>
2019-05-01 19:16:57 +0300

    creating static fn for login user by username

--- src/models/user.js
+++ src/models/user.js
@@ -68,6 +68,18 @@ userSchema.methods.toJSON = function () {
     return userObject
 }
 
+
+userSchema.statics.findByCredentials = async (username, password) => {
+
+    const user = await User.findOne({ username })
+    if (!user) throw new Error('Unable to login')
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) throw new Error('Unable to login')
+
+    return user
+}
+
 // Hash the plain text password before save
 userSchema.pre('save', async function (next) {
     const user = this
