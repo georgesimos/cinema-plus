9b28a35f6a616df0fbb72e0762185195fb2cbbf4
George <admin@georgesimos.com>
2019-05-06 20:13:56 +0300

    disable server info

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose')
 
 const userRouter = require('./routes/users')
 const app = express()
+app.disable('x-powered-by');
 const port = process.env.PORT || 3000
 
 app.use(express.json())
