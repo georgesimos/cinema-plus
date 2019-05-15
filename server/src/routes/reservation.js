e530dc15381f2cd1922fa78a89285363451ef5e9
George <admin@georgesimos.com>
2019-05-15 19:30:49 +0300

    adding reservation router

---
+++ server/src/routes/reservation.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const Reservation = require('../models/reservation')
+
+const router = new express.Router()
+
+// Create a reservation
+router.post('/reservation', async (req, res) => {
+    const reservation = new Reservation(req.body)
+    try {
+        await reservation.save()
+        res.status(201).send(reservation)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Get all reservations
+router.get('/reservation', async (req, res) => {
+    try {
+        const reservations = await Reservation.find({})
+        res.send(reservations)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Get reservation by id 
+router.post('/reservation/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const reservation = await Reservation.findById(_id)
+        !reservation ? res.sendStatus(404) : res.send(reservation)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
+// Update reservation by id
+router.patch('/reservation/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['seats', 'ticketPrice', 'total']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' })
+
+    try {
+        const reservation = await Reservation.findById(_id)
+        updates.forEach((update) => reservation[update] = req.body[update])
+        await reservation.save()
+        !reservation ? res.sendStatus(404) : res.send(reservation)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Delete reservation by id 
+router.delete('/reservation/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const reservation = await Reservation.findByIdAndDelete(_id)
+        !reservation ? res.sendStatus(404) : res.send(reservation)
+    } catch (e) {
+        res.sendStatus(400)
+    }
+})
+
+module.exports = router
