f0e4f9775f4a87932d50546c30305d00ca3424dd
George <admin@georgesimos.com>
2019-09-25 22:52:06 +0300

    test

--- server/src/index.js
+++ server/src/index.js
@@ -41,6 +41,6 @@ app.get('/api/test', (req, res) => res.send('Hello World'))
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
 app.listen(port, () => console.log(`app is running in PORT: ${port}`))
