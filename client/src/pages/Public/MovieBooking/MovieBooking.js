ae03e340cb3af69fb25a1ea2c878b453f02bcf69
George Simos <admin@georgesimos.com>
2019-09-29 13:43:33 +0300

    movieBackground to responsive

--- client/src/pages/Public/MovieBooking/MovieBooking.js
+++ client/src/pages/Public/MovieBooking/MovieBooking.js
@@ -1,14 +1,61 @@
 // @ts-nocheck
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, Box, Typography } from '@material-ui/core';
+import {
+  withStyles,
+  Box,
+  Typography,
+  Grid,
+  Container
+} from '@material-ui/core';
 import Navbar from '../../../layouts/Public/components/Navbar/Navbar';
 
 const styles = theme => ({
   root: {
+    paddingTop: theme.spacing(10),
     backgroundColor: theme.palette.background.dark,
     color: theme.palette.common.white,
     height: '100vh'
+  },
+  container: { height: '100%' },
+  movieInfos: {
+    background: 'rgba(57, 61, 67, 0.5)',
+    position: 'relative',
+    height: '100%'
+  },
+  background: {
+    position: 'absolute',
+    opacity: 0.4,
+    top: 0,
+    height: '70%',
+    right: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    width: '100%',
+    zIndex: 1
+  },
+  title: {
+    position: 'absolute',
+    top: '60%',
+    right: 0,
+    width: '100%',
+    textAlign: 'center',
+    color: theme.palette.common.white,
+    fontSize: '24px',
+    textTransform: 'capitalize',
+    zIndex: 2
+  },
+  info: {
+    position: 'absolute',
+    padding: theme.spacing(5),
+    top: '70%',
+    right: 0,
+    width: '100%'
+  },
+  infoBox: {
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(2)
   },
   row: {
     display: 'flex',
@@ -20,10 +67,20 @@ const styles = theme => ({
     color: 'rgba(255,255,255,0.7)',
     borderRadius: 2,
     padding: theme.spacing(2),
-    margin: theme.spacing(1),
+    margin: theme.spacing(0.5),
     fontWeight: 600,
     '&:hover': {
       background: 'rgb(120, 205, 4)'
+    }
+  },
+  [theme.breakpoints.down('md')]: {
+    root: { height: '100%' },
+    movieInfos: { minHeight: '30vh' },
+    background: { height: '60%' },
+    title: { top: '50%' },
+    info: { top: '60%', display: 'flex', justifyContent: 'center' },
+    infoBox: {
+      marginRight: theme.spacing(2)
     }
   },
   [theme.breakpoints.down('sm')]: {
@@ -97,75 +154,128 @@ class MovieBooking extends Component {
   }
 
   render() {
-    const { cinemaSeats } = this.state;
+    const { movie, cinemaSeats } = this.state;
     const { classes } = this.props;
     return (
       <Fragment>
         <div className={classes.root}>
           <Navbar />
-          <Box width={1} pt={15}>
-            {cinemaSeats.map((seatRows, indexRow) => (
-              <div key={indexRow} className={classes.row}>
-                {seatRows.map((seat, index) => (
+          <Container maxWidth="xl" className={classes.container}>
+            <Grid container spacing={2} style={{ height: '100%' }}>
+              {movie && (
+                <Grid item xs={12} md={12} lg={3}>
+                  <div className={classes.movieInfos}>
+                    <div
+                      className={classes.background}
+                      style={{
+                        backgroundImage: `url(${movie.image})`
+                      }}
+                    />
+                    <Typography className={classes.title}>
+                      {movie.title}
+                    </Typography>
+                    <div className={classes.info}>
+                      {movie.director && (
+                        <div className={classes.infoBox}>
+                          <Typography variant="subtitle1" color="inherit">
+                            Director
+                          </Typography>
+                          <Typography variant="caption" color="inherit">
+                            {movie.director}
+                          </Typography>
+                        </div>
+                      )}
+                      {movie.cast && (
+                        <div className={classes.infoBox}>
+                          <Typography variant="subtitle1" color="inherit">
+                            Cast
+                          </Typography>
+                          <Typography variant="caption" color="inherit">
+                            {movie.cast}
+                          </Typography>
+                        </div>
+                      )}
+                      {movie.genre && (
+                        <div className={classes.infoBox}>
+                          <Typography variant="subtitle1" color="inherit">
+                            Genre
+                          </Typography>
+                          <Typography variant="caption" color="inherit">
+                            {movie.genre}
+                          </Typography>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </Grid>
+              )}
+              <Grid item lg={9} xs={12} md={12}>
+                <Box width={1} pt={15}>
+                  {cinemaSeats.map((seatRows, indexRow) => (
+                    <div key={indexRow} className={classes.row}>
+                      {seatRows.map((seat, index) => (
+                        <Box
+                          key={`seat-${index}`}
+                          onClick={() => this.onSelectSeat(indexRow, index)}
+                          className={classes.seat}
+                          bgcolor={
+                            seat === 1
+                              ? 'rgb(65, 66, 70)'
+                              : seat === 2
+                              ? 'rgb(120, 205, 4)'
+                              : 'rgb(96, 93, 169)'
+                          }>
+                          {index + 1}
+                        </Box>
+                      ))}
+                    </div>
+                  ))}
+                </Box>
+                <Box width={1} mt={10}>
                   <Box
-                    key={`seat-${index}`}
-                    onClick={() => this.onSelectSeat(indexRow, index)}
-                    className={classes.seat}
-                    bgcolor={
-                      seat === 1
-                        ? 'rgb(65, 66, 70)'
-                        : seat === 2
-                        ? 'rgb(120, 205, 4)'
-                        : 'rgb(96, 93, 169)'
-                    }>
-                    {index + 1}
+                    width="50%"
+                    margin="auto"
+                    display="flex"
+                    alignItems="center"
+                    textAlign="center"
+                    color="#eee">
+                    <Typography color="inherit">
+                      <Box
+                        mr={1}
+                        display="inline-block"
+                        width={10}
+                        height={10}
+                        bgcolor="rgb(96, 93, 169)"
+                      />
+                      Seat Available
+                    </Typography>
+                    <Typography color="inherit">
+                      <Box
+                        mr={1}
+                        ml={2}
+                        display="inline-block"
+                        width={10}
+                        height={10}
+                        bgcolor="rgb(65, 66, 70)"
+                      />
+                      Reserved Seat
+                    </Typography>
+                    <Typography color="inherit">
+                      <Box
+                        mr={1}
+                        ml={2}
+                        display="inline-block"
+                        width={10}
+                        height={10}
+                        bgcolor="rgb(120, 205, 4)"
+                      />
+                      Your Seat
+                    </Typography>
                   </Box>
-                ))}
-              </div>
-            ))}
-          </Box>
-          <Box width={1} mt={10}>
-            <Box
-              width="50%"
-              margin="auto"
-              display="flex"
-              alignItems="center"
-              textAlign="center"
-              color="#eee">
-              <Typography color="inherit">
-                <Box
-                  mr={1}
-                  display="inline-block"
-                  width={10}
-                  height={10}
-                  bgcolor="rgb(96, 93, 169)"
-                />
-                Seat Available
-              </Typography>
-              <Typography color="inherit">
-                <Box
-                  mr={1}
-                  ml={2}
-                  display="inline-block"
-                  width={10}
-                  height={10}
-                  bgcolor="rgb(65, 66, 70)"
-                />
-                Reserved Seat
-              </Typography>
-              <Typography color="inherit">
-                <Box
-                  mr={1}
-                  ml={2}
-                  display="inline-block"
-                  width={10}
-                  height={10}
-                  bgcolor="rgb(120, 205, 4)"
-                />
-                Your Seat
-              </Typography>
-            </Box>
-          </Box>
+                </Box>
+              </Grid>
+            </Grid>
+          </Container>
         </div>
         <div className="cursor" id="cursor" />
         <div className="cursor2" id="cursor2" />
