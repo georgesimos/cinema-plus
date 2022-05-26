e21048bf57419f44ba99f4c63c297bb5b35d1641
Kris Chen <kc@krischen.ca>
2022-05-26 14:44:55 -0400

    externalize mail settings variables

--- server/src/utils/mail.js
+++ server/src/utils/mail.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: process.env.MAIL_SERVICE, // smtp.gmail.com  //in place of service use host...
-  secure: process.env.MAIL_SECURED, // true
+  secure: process.env.MAIL_SECURE, // true
   port: process.env.MAIL_PORT, // 465
   auth: {
     user: process.env.MAIL_USER,
